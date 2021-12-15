6e7d405c78eb332ddbff9e0763ead944941d9f3b
Dominika <d.bochniarz@gmail.com>
2021-12-15 16:08:38 +0100

    add input

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
+let num1 = document.getElementById('number-one');
+let num2 = document.getElementById('number-two');
 
-let summText = document.getElementById("sum-el")
+document.getElementById("number-one").textContent = num1;
+document.getElementById("number-two").textContent = num2;
+
+let summText = document.getElementById("sum-el");
 
 function add() {
-    let add = num1 + num2
-    summText.textContent = add
+   let add = parseInt(num1.value) + parseInt(num2.value)
+   summText.textContent = add
 }
 
 function subtract() {
-    let subtract = num1 - num2
+    let subtract = parseInt(num1.value - num2.value);
     summText.textContent = subtract
 }
 
 function divide() {
-    let divide = num1 / num2
+    let divide = parseInt(num1.value / num2.value);
     summText.textContent = divide
 }
 
 function multiply() {
-    let multiply = num1 * num2
+    let multiply = parseInt(num1.value * num2.value);
     summText.textContent = multiply
 }
+
